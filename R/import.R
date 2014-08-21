5a5dab13b98179f6e2cccfe2156193468b131725
Christian Heckendorf <heckendorfc@gmail.com>
2014-08-21 11:38:07 -0400

    Adding support to import from URL.

--- R/import.R
+++ R/import.R
@@ -1,16 +1,24 @@
-.getfile <- function(url){
+.geturldata <- function(url){
+	curlopts <- list(useragent="BTDR",verbose=F,followLocation=T)
 	url <- sub("results.html","cgi-bin/get_results.cgi",url)
-	stop("Reading from URL not yet supported")
+	#stop("Reading from URL not yet supported")
+
+	rawdata <- RCurl::getURL(url,followLocation=T,.opts=curlopts)
+
+	if(rawdata=="")
+		stop("This URL contains no information. Either the URL is invalid or the data is still processing.")
+
+	yaml.load(rawdata)
 }
 
 read.bupid <- function(file=NULL,url=NULL){
 	if(is.null(file) && is.null(url))
-		stop("Either file or url must be given.")
+		stop("Either file or url must be provided.")
 
 	if(is.null(file))
-		file <- .getfile(url)
-
-	res <- yaml.load_file(file)
+		res <- .geturldata(url)
+	else
+		res <- yaml.load_file(file)
 
 	res
 }
