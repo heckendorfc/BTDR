1a5060073bb61c4fe546ddd0d223865ae0cc682c
Christian Heckendorf <heckendorfc@gmail.com>
2017-06-15 12:12:56 -0400

    Add tag.rank to global variables.

--- R/00_globalVariables.r
+++ R/00_globalVariables.r
@@ -1,6 +1,6 @@
 utils::globalVariables(c(
   "peak.id", "protid", "unimododo", "mass", "ppmError", "color",
   "black", "mass", "intensity", "color", "label", "mass", "intensity",
-  "peakid"
+  "peakid", "tag.rank"
 ))
 
