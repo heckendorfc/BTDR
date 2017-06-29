0b8aa5b04799da3389a6d494b21b1f889c8ebb9b
Christian Heckendorf <heckendorfc@gmail.com>
2017-06-29 10:45:51 -0400

    Bundle libyaml 0.1.7.

---
+++ src/libyaml/src/config.h
@@ -0,0 +1,38 @@
+/* Name of package */
+#define PACKAGE "yaml"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://bitbucket.org/xi/libyaml/issues/new"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "yaml"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "yaml 0.1.7"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "yaml"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1.7"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1.7"
+
+/* Define the major version number. */
+#define YAML_VERSION_MAJOR 0
+
+/* Define the minor version number. */
+#define YAML_VERSION_MINOR 1
+
+/* Define the patch version number. */
+#define YAML_VERSION_PATCH 7
+
+/* Define the version string. */
+#define YAML_VERSION_STRING "0.1.7"
